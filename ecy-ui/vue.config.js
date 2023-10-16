bad7335ce92a9fe6f7b047d31195b2e4c81c0057
yongoe <121887765@qq.com>
2023-10-16 17:07:52 +0800

    优化:前端文件加时间戳防止缓存

--- ecy-ui/vue.config.js
+++ ecy-ui/vue.config.js
@@ -1,6 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
-
+const Timestamp = new Date().getTime()
 module.exports = defineConfig({
+  configureWebpack: {
+    output: { // 输出重构，打包编译后的文件名称 【模块名称.版本号(可选).时间戳】
+      filename: `[name].${Timestamp}.js`,
+      chunkFilename: `[name].${Timestamp}.js`
+    },
+  },
+  css: {
+    extract: { // 打包后css文件名称添加时间戳
+      filename: `css/[name].${Timestamp}.css`,
+      chunkFilename: `css/[name].${Timestamp}.css`
+    }
+  },
   transpileDependencies: true,
   lintOnSave: false,
   publicPath: '/',
